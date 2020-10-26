e90b0a4304688ad39553a7312e5854b328a466ed
brunoo1997 <73483052+brunoo1997@users.noreply.github.com>
2020-10-26 11:50:26 -0300

    Add files via upload

---
+++ mipagina/control.js
@@ -0,0 +1,28 @@
+
+
+var btn = document.getElementById('btn');
+var pass = document.getElementById('pass');
+var user = document.getElementById('user');
+var regext = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/
+
+
+btn.addEventListener('click', function(e){
+
+      if(!regext.test(user.value)){
+
+        alert('Debes colocar una dirección de mail valida')
+        e.preventDefault();
+        return false;
+
+      }else if(user.value === ''){
+
+        alert('El campo Usuario es obligatorio!')
+          e.preventDefault();
+          return false;
+
+      }else if(pass.value === ''){
+        alert('El campo contraseña es obligatorio!')
+        e.preventDefault();
+        return false;
+      }
+});
